1d9feb190975eb19fa8e00f242bdd86b52602b1c
babunny248 <134237352+babunny248@users.noreply.github.com>
2024-10-01 16:02:13 +0000

    Frist java Project

---
+++ MiddleThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MiddleThree{
+    public static void main(String[]args){
+        System.out.println("hello World");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("enter your name");
+        String name = keyboard.nextLine();
+
+        System.out.println("hello, " + name);
+
+    }
+}
